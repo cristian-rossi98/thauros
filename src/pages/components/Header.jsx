1faca74a91915cb8ec608add16f3fe87255d3718
Cristian Rossi <cristian.rossicosta@gmail.com>
2023-08-09 04:03:11 -0300

    bug vercel

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -4,16 +4,16 @@ const Header = ({ socialMedia }) => {
   return (
     <header className="h-screen w-screen fixed bg-gray-950 flex flex-col items-center py-40 z-0">
       <div>
-        {socialMedia && socialMedia.map((social, index) => (
+        {socialMedia.map((social, index) => (
           <li className="list-none w-full mb-2" key={index}>
             <a
               href={social.link}
               target="_blank"
               className="flex flex-row items-center"
             >
-              {/* <div className="w-4 mr-4">
+              <div className="w-4 mr-4">
                 <img className="invert" src={social.img} alt={social.title} />
-              </div> */}
+              </div>
               <h1 className="text-xl">{social.title}</h1>
             </a>
           </li>
